1e49fca85bb7a9ed2115df8d75e59fec8ecdcaf1
FelipeGabrill <felipegabrielodss@gmail.com>
2024-09-12 16:35:16 -0300

    Página de sorteio

---
+++ src/paginas/Sorteio.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { RecoilRoot } from "recoil";
+import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
+import Sorteio from "./Sorteio";
+
+jest.mock('../state/hooks/useListaDeParticipantes', () => {
+    return {
+        useListaDeParticipantes: jest.fn()
+    }
+})
+
+describe('Na pagina de sorteio', () => {
+    const participantes = ['Lucas', 'Joao', 'Maria']
+
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes)
+    })
+
+    test('Todos os participantes podem exibir o seu seu amigo secreto', () => {
+        render(<RecoilRoot>
+            <Sorteio />
+        </RecoilRoot>)
+
+        const opcoes = screen.queryAllByRole('option')
+        expect(opcoes).toHaveLength(participantes.length)
+    })
+})
